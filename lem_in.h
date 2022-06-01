ceebcca98da9d10c3ac4f2acac7fa27ca03bb9ec
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2022-06-01 16:00:03 +0300

    Makefile, simple main,  handle_error function and .h file

---
+++ lem_in.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem-in.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-maul <mde-maul@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 15:54:22 by mde-maul          #+#    #+#             */
+/*   Updated: 2022/06/01 15:54:23 by mde-maul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEM_IN_H
+# define LEM_IN_H
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include "libft/includes/libft.h"
+
+void	handle_error(void);
+
+#endif
