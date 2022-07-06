79c8468269f0143c60888f28010162d1a150b728
Baptiste Riffard <briffard@c3r2p7.hive.fi>
2022-07-06 14:56:48 +0300

    Normi parsing_ants|| MIssing headere

--- srcs/parsing_ants.c
+++ srcs/parsing_ants.c
@@ -1,41 +1,42 @@
 #include "parsing.h"
 
-int only_digit(char *line)
+int	only_digit(char *line)
 {
-    int     i;
-    i = 0;
-    while (line[i] != '\0')
-    {
-        if(!ft_isdigit(line[i]))
-            return (FALSE);
-        i++;
-    }
-    return (TRUE);
+	int	i;
+
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if (!ft_isdigit(line[i]))
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
 }
 
-
-int is_nb_ants(char *line)
+int	is_nb_ants(char *line)
 {
-    if (only_digit(line))
-        return (TRUE);
-    return (FALSE);
+	if (only_digit(line))
+		return (TRUE);
+	return (FALSE);
 }
 
-int     get_ants(t_data *data, char *line)
+int	get_ants(t_data *data, char *line)
 {
-    int ants;
+	int	ants;
 
-    ants = 0;
-    while (get_next_line(0, &line))
-    {
-        if(is_nb_ants(line))
-        {
-            ants = ft_atoi(line);
-            ft_strdel(&line);
-            return (ants);
-        }
-        if(is_comment(line) || is_connexion(line, 1) ||( is_room(line, 0) && ants == 0))
-            check_data(data);
-    }
-    return (0);
+	ants = 0;
+	while (get_next_line(0, &line))
+	{
+		if (is_nb_ants(line))
+		{
+			ants = ft_atoi(line);
+			ft_strdel(&line);
+			return (ants);
+		}
+		if (is_comment(line) || is_connexion(line, 1) || \
+		(is_room(line, 0) && ants == 0))
+			check_data(data);
+	}
+	return (0);
 }
