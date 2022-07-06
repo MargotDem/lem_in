d1f0e19b3a7d8cc24372b8939d23b9dbdcf9e517
Baptiste Riffard <briffard@c3r2p7.hive.fi>
2022-07-06 15:17:13 +0300

    Normi hastable_main.c || Header missing

--- srcs/hashtable_main.c
+++ srcs/hashtable_main.c
@@ -3,28 +3,31 @@
 void	create_hashtable(t_data **data)
 {
 	int	i;
+	int	size;
 
 	i = 0;
-	(*data)->hashtab = malloc(sizeof(t_room) * ((*data)->size_lst));
-	if(!(*data)->hashtab)
+	size = (*data)->size_lst;
+	(*data)->hashtab = malloc(sizeof(t_room) * size);
+	if (!(*data)->hashtab)
 		err_handling("malloc");
-	while (i < ((*data)->size_lst))
+	while (i < size)
 	{
-		(*data)->hashtab[i] =  NULL;
+		(*data)->hashtab[i] = NULL;
 		i++;
 	}
 	printf("Hashtable created\n");
 }
 
-
-unsigned long		hashing(char *str, unsigned int size)
+unsigned long	hashing(char *str, unsigned int size)
 {
-	size_t	i;
-	unsigned long h_index = 0;
+	size_t			i;
+	unsigned long	h_index;
+
+	h_index = 0;
 	i = 0;
-    while (str[i] != '\0')
-    {
-		h_index +=  str[i];
+	while (str[i] != '\0')
+	{
+		h_index += str[i];
 		i++;
 	}
 	return (h_index % size);
@@ -32,12 +35,11 @@ unsigned long		hashing(char *str, unsigned int size)
 
 t_room	*insert_room(t_data **data, t_room *li, int index)
 {
-	t_room *temp;
-	t_room *tempo;
-	
+	t_room	*temp;
+	t_room	*tempo;
+
 	tempo = NULL;
 	temp = (*data)->hashtab[index];
-
 	if ((*data)->hashtab[index] == NULL)
 	{
 		temp = li;
@@ -51,17 +53,17 @@ t_room	*insert_room(t_data **data, t_room *li, int index)
 		temp->h_next = li;
 		li->h_next = NULL;
 	}
-
 	return (tempo);
 }
 
-
 void	print_hashtab(t_data **data)
 {
-	int i = 0;
-	t_room *temp = NULL;
+	int		i;
+	t_room	*temp;
 
-	while ( i < (*data)->size_lst)
+	i = 0;
+	temp = NULL;
+	while (i < (*data)->size_lst)
 	{
 		temp = (*data)->hashtab[i];
 		if (temp != NULL)
@@ -79,18 +81,17 @@ void	print_hashtab(t_data **data)
 void	fill_up_hashtable(t_data **data, t_room *li)
 {
 	unsigned int	hash_index;
-	t_room	*temp;
+	t_room			*temp;
 
 	temp = li;
 	hash_index = 0;
 	while (temp != NULL)
 	{
-		hash_index = hashing(temp->name, (*data)->size_lst );
+		hash_index = hashing(temp->name, (*data)->size_lst);
 		(*data)->hashtab[hash_index] = insert_room(data, temp, hash_index);
 		temp = temp->next;
 	}
 }
-
 
 void	hashtable_main(t_data **data, t_room *li)
 {
