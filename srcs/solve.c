b5c87945663f7dffd5a1f1879936ccc11388f474
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2022-07-26 21:28:47 +0300

    faster solution: for each new augmenting path, calculate straight away if it gives better solution and if yes, stop looking for more augm paths

--- srcs/solve.c
+++ srcs/solve.c
@@ -245,20 +245,43 @@ static	void	get_paths(t_all_paths_combos *all_paths_combos, t_room *graph, t_dat
 	all_paths_combos->counter++;
 }
 
+void	calc_solution(t_paths **solution, t_all_paths_combos *all_paths_combos, size_t nb_ants)
+{
+	int	k;
+	size_t	shortest_nb_turns;
+
+	k = 1; // what if there is not even one path combination???
+	distribute_ants(all_paths_combos->arr[0], nb_ants);
+	shortest_nb_turns = all_paths_combos->arr[0]->path_size + all_paths_combos->arr[0]->nb_ants;
+	*solution = all_paths_combos->arr[0];
+	while (k < all_paths_combos->counter)
+	{
+		distribute_ants(all_paths_combos->arr[k], nb_ants);
+		if (all_paths_combos->arr[k]->path_size + all_paths_combos->arr[k]->nb_ants < shortest_nb_turns)
+		{
+			shortest_nb_turns = all_paths_combos->arr[k]->path_size + all_paths_combos->arr[k]->nb_ants;
+			*solution = all_paths_combos->arr[k];
+		}
+		k++;
+	}
+}
+
 void	solve222(t_room *graph, t_data *data, char *start, char *end)
 {
 	t_hist	*path;
 	t_all_paths_combos	*all_paths_combos;
 	int	x;
 	int	i;
-	size_t	shortest_nb_turns;
+	
 	t_paths *solution;
+	t_paths *solution_tmp;
 	size_t	nb_ants;
+	size_t	prev_nb_turns;
 	char	*start_and_end[2];
-	int	k;
 
 	nb_ants = data->ants;
 	x = 0;
+	prev_nb_turns = 5000;
 	all_paths_combos = (t_all_paths_combos *)malloc(sizeof(t_all_paths_combos)); // if null
 	all_paths_combos->size = 200;
 	all_paths_combos->counter = 0;
@@ -290,9 +313,17 @@ void	solve222(t_room *graph, t_data *data, char *start, char *end)
 		}
 		//printf("\n\n\n iteration number %d, now all the paths are\n", x + 1);
 		get_paths(all_paths_combos, graph, data);
+		calc_solution(&solution_tmp, all_paths_combos, nb_ants);
+		if (prev_nb_turns > solution_tmp->path_size + solution_tmp->nb_ants - 2)
+		{
+			solution = solution_tmp;
+			prev_nb_turns = solution_tmp->path_size + solution->nb_ants - 2;
+		}
+		else
+			break ;
+	
 		x++;
 	}
-
 
 	/*printf("\n\n\n\n\n\n\n\n\nALRIGHTTT\nall paths combos counter is %d", all_paths_combos->counter);
 	printf(", and all paths combos is:\n");
@@ -308,27 +339,12 @@ void	solve222(t_room *graph, t_data *data, char *start, char *end)
 
 	*/
 	
-
-	k = 1; // what if there is not even one path combination???
-	distribute_ants(all_paths_combos->arr[0], nb_ants);
-	shortest_nb_turns = all_paths_combos->arr[0]->path_size + all_paths_combos->arr[0]->nb_ants;
-	solution = all_paths_combos->arr[0];
-	while (k < all_paths_combos->counter)
-	{
-		distribute_ants(all_paths_combos->arr[k], nb_ants);
-		if (all_paths_combos->arr[k]->path_size + all_paths_combos->arr[k]->nb_ants < shortest_nb_turns)
-		{
-			shortest_nb_turns = all_paths_combos->arr[k]->path_size + all_paths_combos->arr[k]->nb_ants;
-			solution = all_paths_combos->arr[k];
-		}
-		k++;
-	}
-	start_and_end[0] = start;
-	start_and_end[1] = end;
 	printf("\n\nmmkayyyy is this the right solution tho:\n\n");
 	print_paths(solution);
 	display_result(solution, nb_ants);
 	printf("number of turns is %zu\n", solution->path_size + solution->nb_ants - 2);
+	start_and_end[0] = start;
+	start_and_end[1] = end;
 	//visualizer(graph, nb_ants, solution, start_and_end);
 }
 
