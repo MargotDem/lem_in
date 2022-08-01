55fafe1a1ddb5c2000d780e797a366e4aed50257
Baptiste Riffard <briffard@c1r6p18.hive.fi>
2022-08-01 11:59:17 +0300

    malloc_hanlder.c norme

--- srcs/malloc_handler.c
+++ srcs/malloc_handler.c
@@ -1,39 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc_handler.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: briffard <briffard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 11:54:25 by briffard          #+#    #+#             */
+/*   Updated: 2022/08/01 11:58:41 by briffard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parsing.h"
 
-t_data  *create_data()
+t_data	*create_data(void)
 {
-    t_data *element;
+	t_data	*element;
 
-    element = (t_data *)malloc(sizeof(*element));
-    // printf("DATA ---> %p\n", element);
-    if (!element)
-        panic("malloc data");
-    return (element);
+	element = (t_data *)malloc(sizeof(*element));
+	if (!element)
+		panic("In malloc_handler.c: create_data");
+	return (element);
 }
 
-t_room  *new_node(char *line, char letter)
+t_room	*new_node(char *line, char letter)
 {
-    t_room  *new;
+	t_room	*new;
 
-    new = (t_room *)malloc(sizeof(*new));
-    // printf("new node ---> %p\n", new);
-    if (!new)
-        panic("malloc new_node");
-    new = set_room(line, letter, new);
-    return (new);
+	new = (t_room *)malloc(sizeof(*new));
+	if (!new)
+		panic("I malloc_handler: new_node");
+	new = set_room(line, letter, new);
+	return (new);
 }
 
 void	create_hashtable(t_data **data)
 {
-	size_t	i;
+	size_t			i;
 	unsigned long	size;
 
 	i = 0;
 	size = (*data)->size_list;
 	(*data)->hashtab = (t_room **)malloc(sizeof(*(*data)->hashtab) * size);
-    // printf("HAAASSHHH ---> %p\n", (*data)->hashtab);
 	if (!(*data)->hashtab)
-		panic("malloc hashtab");
+		panic("In malloc_handler: create_hashtable");
 	while (i < size)
 	{
 		(*data)->hashtab[i] = NULL;
@@ -41,19 +50,17 @@ void	create_hashtable(t_data **data)
 	}
 }
 
-
-t_room **brealloc(t_room **links,t_room *to, unsigned long size_list)
+t_room	**brealloc(t_room **links, t_room *to, unsigned long size_list)
 {
-	size_t x;
-	t_room **dst;
+	size_t	x;
+	t_room	**dst;
 
 	x = 0;
 	dst = (t_room **)malloc(sizeof(t_room *) * size_list + sizeof(t_room *));
-    // printf("dest --->%p\n", dst);
 	if (!dst)
-		panic("reallocation memory");
-	while ( x < size_list )
-	{   
+		panic("In malloc_handler: brealloc");
+	while (x < size_list)
+	{
 		dst[x] = links[x];
 		x++;
 	}
@@ -63,32 +70,10 @@ t_room **brealloc(t_room **links,t_room *to, unsigned long size_list)
 	return (dst);
 }
 
-
-char	*cpy_line(char *line, char *test, t_data *data)
+void	create_links(t_room **room)
 {
-	char	*temp;
-	char	*map;
-
-	(void)test;
-	map = data->map;
-	if (!map)
-		temp = ft_strdup(line);
-	else
-		temp = ft_strjoin(map, line);
-	if (!temp)
-		panic("In malloc_handler.c: cpy_line");
-	free(map);
-	map = ft_strjoin(temp,"\n");
-	ft_strdel(&temp);
-	data->size_map += ft_strlen(line) + 1;
-	return (map);
-}
-
-void    create_links(t_room **room)
-{
-    (*room)->links = (t_room **)malloc(sizeof(t_room *));
-    // printf("LINKS -->%p\n", (*room)->links);
-    if (!(*room)->links)
-        panic("malloc links");
-    (*room)->links[0] = NULL;
+	(*room)->links = (t_room **)malloc(sizeof(t_room *));
+	if (!(*room)->links)
+		panic("In malloc_handler: create_links");
+	(*room)->links[0] = NULL;
 }
