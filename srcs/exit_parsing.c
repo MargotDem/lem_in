2298402be9f7875f7ee20bd8f9a7a3f612566610
Baptiste Riffard <briffard@c1r6p18.hive.fi>
2022-08-01 11:39:27 +0300

    exit_parsing.c norme

--- srcs/exit_parsing.c
+++ srcs/exit_parsing.c
@@ -1,29 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_parsing.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: briffard <briffard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 11:37:11 by briffard          #+#    #+#             */
+/*   Updated: 2022/08/01 11:38:47 by briffard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parsing.h"
 
-unsigned short data_is_ok(t_data *data)
+unsigned short	data_is_ok(t_data *data)
 {
-    if (data->ants == 0)
-        return (FALSE);
-    if (!data->end_room_name || !data->start_room_name)
-        return (FALSE);
-    return (TRUE);
+	if (data->ants == 0)
+		return (FALSE);
+	if (!data->end_room_name || !data->start_room_name)
+		return (FALSE);
+	return (TRUE);
 }
 
-void    exit_parsing(t_room *rooms, t_data *data)
+void	exit_parsing(t_room *rooms, t_data *data)
 {
-    if (rooms && data->hashtable_created == 0)
-        while(rooms != NULL)
-		    rooms = my_clean(rooms);
-    if (data_is_ok(data))
-    {
-        write(1, data->map, data->index_line);
-        printf("go_to_solver\n");
-        data = data_cleaner(data);
-        system("leaks lem-in");
-        exit(EXIT_SUCCESS);
-    }
-    ft_putstr("error\n"); 
-    data = data_cleaner(data);
-    system("leaks lem-in");
-    exit(EXIT_FAILURE);
+	if (rooms && data->hashtable_created == 0)
+		while (rooms != NULL)
+			rooms = my_clean(rooms);
+	if (data_is_ok(data))
+	{
+		if (data->no_map == FALSE)
+			write(1, data->map, data->index_line);
+		printf("go_to_solver\n");
+		data = data_cleaner(data);
+		system("leaks lem-in");
+		exit(EXIT_SUCCESS);
+	}
+	ft_putstr("error\n");
+	data = data_cleaner(data);
+	system("leaks lem-in");
+	exit(EXIT_FAILURE);
 }
