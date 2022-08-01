3b5f6443ef4f13540f3a1f6d88509f43409e90d3
Baptiste Riffard <briffard@c1r6p18.hive.fi>
2022-08-01 13:08:30 +0300

    save_data_tool.c norme

--- srcs/save_data_tools.c
+++ srcs/save_data_tools.c
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   save_data_tools.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: briffard <briffard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 13:07:41 by briffard          #+#    #+#             */
+/*   Updated: 2022/08/01 13:08:15 by briffard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parsing.h"
 
-void create_link(t_room *room_1, t_room *room_2)
+void	create_link(t_room *room_1, t_room *room_2)
 {
-	
-	if(room_1->links[0] == NULL)
+	if (room_1->links[0] == NULL)
 	{
 		room_1->links[0] = room_2;
 		room_1->total_links = 1;
