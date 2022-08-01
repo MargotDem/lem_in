d83ad540d0ce173a0876605d7379735c5cf88832
Baptiste Riffard <briffard@c1r6p18.hive.fi>
2022-08-01 12:16:28 +0300

    init_value.c norme

--- srcs/init_value.c
+++ srcs/init_value.c
@@ -1,35 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_value.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: briffard <briffard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 12:14:09 by briffard          #+#    #+#             */
+/*   Updated: 2022/08/01 12:16:06 by briffard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parsing.h"
 
-t_data  *set_data(t_data *node)
+t_data	*set_data(t_data *node)
 {
-    node->ants = 0;
-    node->help = 0;
-    node->visual = 0;
-    node->no_map = 0;
-    node->end_room_name = NULL;
-    node->start_room_name = NULL;
-    node->hashtable_created = FALSE;
-    node->section_links = DIACTIVATE;
-    node->section_rooms = DIACTIVATE;
-    node->map = NULL;
-    node->index_line = 0;
+	node->ants = 0;
+	node->help = 0;
+	node->visual = 0;
+	node->no_map = 0;
+	node->end_room_name = NULL;
+	node->start_room_name = NULL;
+	node->hashtable_created = FALSE;
+	node->section_links = DIACTIVATE;
+	node->section_rooms = DIACTIVATE;
+	node->map = NULL;
+	node->index_line = 0;
 	node->size_list = 0;
-    return (node);
+	return (node);
 }
 
-t_room  *set_room(char *line, char letter, t_room *room)
+t_room	*set_room(char *line, char letter, t_room *room)
 {
-    room->name = save_name(line);
-    room->start = FALSE;
-    room->end = FALSE;
-    room->abscisse = save_abscisse(line);
-    room->ordonne = save_ordonne(line);
-    room->next = NULL;
-    room->hash_next = NULL;
-    room->links = NULL;
-    if (letter =='s')
-        room->start = TRUE;
-    else if (letter == 'e')
-        room->end = TRUE;
-    return (room);
+	room->name = save_name(line);
+	if (!room->name)
+		panic("In init_value: set_room");
+	room->start = FALSE;
+	room->end = FALSE;
+	room->abscisse = save_abscisse(line);
+	room->ordonne = save_ordonne(line);
+	room->next = NULL;
+	room->hash_next = NULL;
+	room->links = NULL;
+	if (letter == 's')
+		room->start = TRUE;
+	else if (letter == 'e')
+	room->end = TRUE;
+	return (room);
 }
