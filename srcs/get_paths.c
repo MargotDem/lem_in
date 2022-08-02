377838381274b7a883a67ca68667d4f8c76be0d4
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2022-08-02 19:04:25 +0300

    norm get_paths.c

--- srcs/get_paths.c
+++ srcs/get_paths.c
@@ -12,59 +12,56 @@
 
 #include "lem_in.h"
 
-void	get_paths(t_all_paths_combos *all_paths_combos, t_room *graph, t_data *data)
+void	create_path_node(t_path_node **path_node, t_room *node)
 {
-	int	i;
-	int	j;
-	t_room	*end;
-	t_room	*node;
-	t_paths *path_el;
-	t_paths *paths;
-	t_path_node *path_node;
-	size_t	path_size;
+	(*path_node) = (t_path_node *)handle_null(malloc(sizeof(t_path_node)));
+	(*path_node)->next = NULL;
+	(*path_node)->node = node;
+}
+
+void	get_path(t_room *node, t_paths **paths, t_room *graph, t_room *end)
+{
+	t_paths			*path_el;
+	t_path_node		*path_node;
+	size_t			path_size;
+
+	path_el = create_path_el();
+	path_size = 2;
+	create_path_node(&path_node, end);
+	path_el->path = path_node;
+	while (node && node != graph)
+	{
+		path_size++;
+		create_path_node(&path_node, node);
+		push_front(&(path_el->path), path_node);
+		node = node->reverse;
+	}
+	if (node != graph)
+		ft_putstr("ALERT so here we have a path that ends \
+			with a node other than start, this should NEVER happen\n");
+	create_path_node(&path_node, graph);
+	push_front(&(path_el->path), path_node);
+	path_el->path_size = path_size;
+	if (!(*paths))
+		(*paths) = path_el;
+	else
+		lst_add_in_order(paths, path_el);
+}
+
+void	get_paths(t_all_paths_combos *all_paths_combos, \
+	t_room *graph, t_data *data)
+{
+	int				i;
+	t_room			*end;
+	t_paths			*paths;
 
 	i = 0;
-	paths = NULL;
 	end = search_for(data->exit_room, data);
+	paths = NULL;
 	while (i < end->nb_links)
 	{
 		if (end->links[i]->reverse)
-		{
-			path_el = create_path_el();
-			path_size = 2;
-			path_node = (t_path_node *)malloc(sizeof(t_path_node));
-			if (!path_node)
-				handle_error();
-			path_node->next = NULL;
-			path_node->node = end;
-			path_el->path = path_node;
-			j = 0;
-			node = end->links[i];
-			while (node && node != graph)
-			{
-				path_size++;
-				path_node = (t_path_node *)malloc(sizeof(t_path_node));
-				if (!path_node)
-					handle_error();
-				path_node->next = NULL;
-				path_node->node = node;
-				push_front(&(path_el->path), path_node);
-				node = node->reverse;
-			}
-			if (node != graph)
-				ft_putstr("ALERT so here we have a path that ends with a node other than start, this should NEVER happen\n");
-			path_node = (t_path_node *)malloc(sizeof(t_path_node));
-			if (!path_node)
-				handle_error();
-			path_node->next = NULL;
-			path_node->node = graph;
-			push_front(&(path_el->path), path_node);
-			path_el->path_size = path_size;
-			if (!paths)
-				paths = path_el;
-			else
-				lst_add_in_order(&paths, path_el);
-		}
+			get_path(end->links[i], &paths, graph, end);
 		i++;
 	}
 	all_paths_combos->arr[all_paths_combos->counter] = paths;
