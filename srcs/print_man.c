cd750fe8992f7e401234fcb620ae5d2f2b19d8ed
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2022-10-05 16:56:07 +0300

    man text modif

--- srcs/print_man.c
+++ srcs/print_man.c
@@ -31,22 +31,22 @@ void	print_man(void)
 	print_bold("\nSYNOPSIS\n\t");
 	print_usage();
 	print_bold("\nDESCRIPTION\n");
-	ft_putstr("\tWe make an ant farm, with tunnels and rooms. We place the ants\
-			on one side, and we look at how they find the exit.\n");
+	ft_putstr("\tWe make an ant farm, with tunnels and rooms. We place the ants \
+on one side, and we look at how they find the exit.\n");
 	ft_putstr("\tWe join the rooms to one another with as many tubes as we \
-			need. A tube joins two rooms to each other.\n");
+need. A tube joins two rooms to each other.\n");
 	ft_putstr("\tA room can be linked to an infinite number of rooms and by as \
-			many tubes as deemed necessary.\nThen we must bury the ant farm \
-			(wherever you want).\nThis is extremely important to ensure that \
-			ants don’t cheat and look at your design before before the game \
-			starts.\n");
+many tubes as deemed necessary.\n\tThen we must bury the ant farm \
+(wherever you want).\n\tThis is extremely important to ensure that \
+ants don’t cheat and look at your design before before the game \
+starts.\n");
 	ft_putstr("\nThe following options are available:\n\n");
 	print_bold("\t-m");
-	ft_putstr("\tDoesn't print the map on the standart ouput.\n");
+	ft_putstr("\tDoesn't print the map on the standard output.\n");
 	print_bold("\t-v");
-	ft_putstr("\tActivate the graphic mode.\n");
+	ft_putstr("\tActivates the graphic mode.\n");
 	print_bold("\t-p");
-	ft_putstr("\tPrint the paths taken by the ants.\n");
+	ft_putstr("\tPrints the paths taken by the ants.\n");
 	print_bold("\t-h");
-	ft_putstr("\tDisplay man of the program.\n\n");
+	ft_putstr("\tDisplays man of the program.\n\n");
 }
