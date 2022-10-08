d9c96acba1679f8acede62ae5bb78d2ec8fac376
B9R9 <baptiste.riffard@gmail.com>
2022-10-08 17:46:10 +0300

    Update lem-in_tester

--- lem-in_tester.sh
+++ lem-in_tester.sh
@@ -43,6 +43,7 @@ declare -a map
 declare -a your_line
 declare -a ref_line
 declare -a ref_leaks
+declare -a diff_arr
 
 clean () {
     if [ -e $pwd/leaks ];then
@@ -78,42 +79,28 @@ save_leaks () {
 }
 
 set_data_leaks () {
-    # process_line=$(grep "leaked bytes" $pwd/exit_leaks.txt)
-    # leaks=$(grep "leaked bytes" $pwd/exit_leaks.txt | cut -d ' ' -f 3)
     process_line=$(grep "leaked bytes" $1)
     leak=$(grep "leaked bytes" $1 | cut -d ' ' -f 3)
 }
 
 save_arr () {
-    # map=( "${map[@]}" "$strmap" )
-    # your_line=( "${your_line[@]}" "ERROR" )
-    # ref_line=( "${ref_line[@]}" "ERROR" )
-    # leak=( "${ref_leaks[@]}" "$leaks" )
     map=( "${map[@]}" "$1" )
     your_line=( "${your_line[@]}" "$2" )
     ref_line=( "${ref_line[@]}" "$3" )
-    leak=( "${ref_leaks[@]}" "$4" )
+    ref_leak=( "${ref_leak[@]}" "$4" )
+    diff_arr=( "${diff_arr[@]}" "$5" )
 }
 
 
 display_error () {
     printf "We found an Error in $1\n"
-    # printf "We found "
-    # if [ $leak -ne 0 ]; then
-    #     printf "${rougefonce}${bold}"
-    #     mkdir -p $pwd/leaks
-    #     cp $pwd/leaks.txt $pwd/leaks/$strmap
-    # else
-    #     printf "${vertfonce}${bold}"   
-    # fi
-    # printf "$leak${neutre} leaks from your program.\n"
-    # printf "\t${italic}$process_line${neutre}\n\n"
 }
 
 set_data_line () {
         line_required=$(grep "lines required" $1 | cut -d' ' -f 8 | head -n 1)
         line_founded=$(grep "^L" $1 | wc -l | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print$0, ""}')
 }
+
 
 display () {
     printf "In map $1\n"
@@ -158,7 +145,7 @@ do
         error=$(grep -i "ERROR" $pwd/map_output/$strmap)
         if [ $error ]; then
             set_data_leaks $pwd/exit_leaks.txt
-            save_arr "$strmap" "Error" "Error" "$leak"
+            save_arr "$strmap" "Error" "Error" $leak "0"
             if [ "$1" = '-v' ]; then
                 display_error "$strmap"
                 display_leaks $leak "$process_line"
@@ -184,17 +171,130 @@ do
                 display $strmap $line_founded $line_required
                 display_leaks $leak "$process_line"
             fi
-            save_arr "$strmap" "$line_founded" "$line_required" "$leak"
+            save_arr "$strmap" "$line_founded" "$line_required" $leak $diff
         fi
     fi
 done
 i=0
 size=${#your_line[*]}
 printf "Summary: \n"
-printf "MAP NAME| SOLUTION | REQUIRED | LEAKS |\n"
+
+option="MAP NAME"
+printf "| ${bold}${option}"
+strsize=${#option}
+while [ "$strsize" -lt "40" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+
+option="SOL"
+printf "| ${bold}${option}"
+strsize=${#option}
+while [ "$strsize" -lt "6" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+
+option="REF"
+printf "| ${bold}${option}"
+strsize=${#option}
+while [ "$strsize" -lt "6" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+
+option="DIFF"
+printf "| ${bold}${option}"
+strsize=${#option}
+while [ "$strsize" -lt "6" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+
+option="LEAKS"
+printf "| ${bold}${option}"
+strsize=${#option}
+while [ "$strsize" -lt "8" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+
+option="RESULTS"
+printf "| ${bold}${option}"
+strsize=${#option}
+while [ "$strsize" -lt "10" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+printf "|${neutre}\n"
+
 while [ "$i" -lt "$size" ]
 do
-printf "${map[i]} | ${your_line[i]} | ${ref_line[i]} | ${leaks[i]} | OK\n"
+printf "| ${map[i]}"
+strsize=${#map[i]}
+while [ "$strsize" -lt "40" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+
+printf "| ${your_line[i]}"
+strsize=${#your_line[i]}
+while [ "$strsize" -lt "6" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+
+printf "| ${ref_line[i]}"
+strsize=${#ref_line[i]}
+while [ "$strsize" -lt "6" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+
+printf "| "
+if [ "${diff_arr[i]}" -gt "5" ]; then
+    printf "${rougefonce}${bold}"  
+else
+    printf "${vertfonce}${bold}"
+fi 
+printf "${diff_arr[i]}${neutre}"
+strsize=${#diff_arr[i]}
+while [ "$strsize" -lt "6" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+
+printf "| "
+if [ "${ref_leak[i]}" -eq "0" ]; then
+    printf "${vertfonce}${bold}"
+else
+    printf "${rougefonce}${bold}" 
+fi
+printf "${ref_leak[i]}${neutre}"
+strsize=${#ref_leak[i]}
+while [ "$strsize" -lt "8" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+if [ "${diff_arr[i]}" -le "5" ]; then
+    if [ "${ref_leak[i]}" -eq "0" ]; then
+        ret="SUCCESS"
+    fi
+else
+    ret="FAIL"
+fi
+if [ "$ret" = "SUCCESS" ]; then
+    printf "| ${vertfonce}${bold}"
+else
+    printf "| ${rougefonce}${bold}"  
+fi
+printf "${ret}${neutre}"
+strsize=${#ret}
+while [ "$strsize" -lt "10" ];do
+    printf " "
+    strsize=$(( $strsize + 1 ))
+done
+printf "|\n"
 i=$(( i + 1 ))
 done
 rm -rf exit_p_leaks.txt
